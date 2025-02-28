824556ade262625099df98bf4892c81b7b285655
Matthew Elliott <m.elliott091091@gmail.com>
2025-02-28 19:13:10 +0000

    Editied database to include images to have a season image that can be edited later. More styling changes to mudcards

---
+++ NeilSeniorBirdWalks/Data/Migrations/20250228174501_AddSeasonImagePathsToLocation.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NeilSeniorBirdWalks.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSeasonImagePathsToLocation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AutumnImagePath",
+                table: "Locations",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "SpringImagePath",
+                table: "Locations",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "SummerImagePath",
+                table: "Locations",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "WinterImagePath",
+                table: "Locations",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AutumnImagePath",
+                table: "Locations");
+
+            migrationBuilder.DropColumn(
+                name: "SpringImagePath",
+                table: "Locations");
+
+            migrationBuilder.DropColumn(
+                name: "SummerImagePath",
+                table: "Locations");
+
+            migrationBuilder.DropColumn(
+                name: "WinterImagePath",
+                table: "Locations");
+        }
+    }
+}
--- NeilSeniorBirdWalks/Models/Location.cs
+++ NeilSeniorBirdWalks/Models/Location.cs
@@ -6,6 +6,10 @@
         public string LocationCode { get; set; }
         public string LocationName { get; set; }
         public string Description { get; set; }
+        public string SpringImagePath { get; set; }
+        public string SummerImagePath { get; set; }
+        public string AutumnImagePath { get; set; }
+        public string WinterImagePath { get; set; }
 
         public ICollection<Tour> Tours { get; set; }    
     }
