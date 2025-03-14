79eb83dd550e4c3b3694391408443cbcee346f88
Matthew Elliott <m.elliott091091@gmail.com>
2025-03-14 15:15:46 +0000

    Removed dbinitializer

--- NeilSeniorBirdWalks/Program.cs
+++ NeilSeniorBirdWalks/Program.cs
@@ -64,21 +64,6 @@ builder.Services.AddMudServices();
 
 var app = builder.Build();
 
-using (var scope = app.Services.CreateScope())
-{
-    var services = scope.ServiceProvider;
-    try
-    {
-        var context = services.GetRequiredService<ApplicationDbContext>();
-        DbInitializer.Initialize(context);
-    }
-    catch (Exception ex)
-    {
-        var logger = services.GetRequiredService<ILogger<Program>>();
-        logger.LogError(ex, "An error occurred while seeding the database.");
-    }
-}
-
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
