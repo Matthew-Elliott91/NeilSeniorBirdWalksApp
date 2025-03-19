06a3e6662bfbda7d25cdc36e6363bb0d80f11bab
Matthew Elliott <m.elliott091091@gmail.com>
2025-03-19 12:14:00 +0000

    Created MyTours basic skeleton

---
+++ NeilSeniorBirdWalks/Data/Migrations/20250319115821_AddedCorrectIdNameToBookingModel.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NeilSeniorBirdWalks.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedCorrectIdNameToBookingModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Bookings_TourSchedules_TourScheduleID",
+                table: "Bookings");
+
+            migrationBuilder.RenameColumn(
+                name: "TourScheduleID",
+                table: "Bookings",
+                newName: "TourScheduleId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Bookings_TourScheduleID",
+                table: "Bookings",
+                newName: "IX_Bookings_TourScheduleId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Bookings_TourSchedules_TourScheduleId",
+                table: "Bookings",
+                column: "TourScheduleId",
+                principalTable: "TourSchedules",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Bookings_TourSchedules_TourScheduleId",
+                table: "Bookings");
+
+            migrationBuilder.RenameColumn(
+                name: "TourScheduleId",
+                table: "Bookings",
+                newName: "TourScheduleID");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Bookings_TourScheduleId",
+                table: "Bookings",
+                newName: "IX_Bookings_TourScheduleID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Bookings_TourSchedules_TourScheduleID",
+                table: "Bookings",
+                column: "TourScheduleID",
+                principalTable: "TourSchedules",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
--- NeilSeniorBirdWalks/Models/Booking.cs
+++ NeilSeniorBirdWalks/Models/Booking.cs
@@ -4,9 +4,9 @@
     {
         public int Id { get; set; }
         public string UserId { get; set; }
-        public int TourScheduleID { get; set; }
-        public DateTime BookingDate { get; set; }
-        public int NumberOfParticipants { get; set; }
+        public int TourScheduleId { get; set; }
+        public DateTime BookingDate { get; set; } = DateTime.UtcNow;
+        public int NumberOfParticipants { get; set; } = 1;
 
         // Navigation Properties
 
