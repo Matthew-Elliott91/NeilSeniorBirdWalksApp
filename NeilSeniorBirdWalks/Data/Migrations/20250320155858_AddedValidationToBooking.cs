32158bceb0e83971b299580e82c4a31a5d0b318e
Matthew Elliott <m.elliott091091@gmail.com>
2025-03-20 16:00:54 +0000

    Added validation to booking model

---
+++ NeilSeniorBirdWalks/Data/Migrations/20250320155858_AddedValidationToBooking.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NeilSeniorBirdWalks.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedValidationToBooking : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "BlogPosts",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedDate",
+                value: new DateTime(2025, 3, 20, 15, 58, 58, 385, DateTimeKind.Utc).AddTicks(5508));
+
+            migrationBuilder.UpdateData(
+                table: "BlogPosts",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedDate",
+                value: new DateTime(2025, 3, 20, 15, 58, 58, 385, DateTimeKind.Utc).AddTicks(5513));
+
+            migrationBuilder.UpdateData(
+                table: "BlogPosts",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedDate",
+                value: new DateTime(2025, 3, 20, 15, 58, 58, 385, DateTimeKind.Utc).AddTicks(5515));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "BlogPosts",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedDate",
+                value: new DateTime(2025, 3, 20, 15, 3, 43, 701, DateTimeKind.Utc).AddTicks(9559));
+
+            migrationBuilder.UpdateData(
+                table: "BlogPosts",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedDate",
+                value: new DateTime(2025, 3, 20, 15, 3, 43, 701, DateTimeKind.Utc).AddTicks(9563));
+
+            migrationBuilder.UpdateData(
+                table: "BlogPosts",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedDate",
+                value: new DateTime(2025, 3, 20, 15, 3, 43, 701, DateTimeKind.Utc).AddTicks(9565));
+        }
+    }
+}
--- NeilSeniorBirdWalks/Models/Booking.cs
+++ NeilSeniorBirdWalks/Models/Booking.cs
@@ -1,16 +1,33 @@
-﻿namespace NeilSeniorBirdWalks.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NeilSeniorBirdWalks.Models
 {
     public class Booking
     {
+        public Booking()
+        {
+            UserId = string.Empty;
+            BookingDate = DateTime.UtcNow;
+            NumberOfParticipants = 1;
+            TourScheduleId = 0;
+        }
+
         public int Id { get; set; }
-        public string UserId { get; set; }
-        public int TourScheduleId { get; set; }
+        
+        [Required] 
+        public required string UserId { get; set; }
+
+        [Required]
+        public required int TourScheduleId { get; set; }
         public DateTime BookingDate { get; set; } = DateTime.UtcNow;
-        public int NumberOfParticipants { get; set; } = 1;
+
+        [Required]
+        [Range(1, 8, ErrorMessage ="Number of participants must be between 1 and 8")]
+        public required int NumberOfParticipants { get; set; } = 1;
 
         // Navigation Properties
 
-        public TourSchedule TourSchedule { get; set; }
+        public TourSchedule? TourSchedule { get; set; }
 
     }
 }
