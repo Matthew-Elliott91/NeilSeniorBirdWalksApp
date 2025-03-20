4f30d9027cab1b25dea809342b19971b324c4eea
Matthew Elliott <m.elliott091091@gmail.com>
2025-03-20 14:17:54 +0000

    Updatead Bird Model validation

---
+++ NeilSeniorBirdWalks/Data/Migrations/20250320141016_UpdatedValidationOnBird.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NeilSeniorBirdWalks.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatedValidationOnBird : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Birds",
+                type: "nvarchar(500)",
+                maxLength: 500,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CommonName",
+                table: "Birds",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Birds",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(500)",
+                oldMaxLength: 500);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "CommonName",
+                table: "Birds",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+        }
+    }
+}
--- NeilSeniorBirdWalks/Models/Bird.cs
+++ NeilSeniorBirdWalks/Models/Bird.cs
@@ -1,12 +1,27 @@
-﻿namespace NeilSeniorBirdWalks.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NeilSeniorBirdWalks.Models
 {
     public class Bird
     {
         public int BirdId { get; set; }
-        public string CommonName { get; set; }
-        public string Description { get; set; }
-        public string ImageUrl { get; set; }
 
-        public ICollection<TourBird> TourBirds { get; set; }
+        [Required(ErrorMessage = "Common name is required")]
+        [StringLength(50, ErrorMessage = "Common name must be less than 50 characters")]
+        [Display(Name = "Common Name")]
+        public required string CommonName { get; set; }
+
+        [Required(ErrorMessage ="Description is required")]
+        [StringLength(500, ErrorMessage = "Description must be less than 500 characters")]
+        [DataType(DataType.MultilineText)]
+        public required string Description { get; set; }
+
+        [Required(ErrorMessage = "Image URL is required")]
+        [Url(ErrorMessage ="Please enter a valid URL")]
+        [Display(Name = "Image URL")]
+        public required string ImageUrl { get; set; }
+
+        // Navigation properties
+        public ICollection<TourBird>? TourBirds { get; set; }
     }
 }
