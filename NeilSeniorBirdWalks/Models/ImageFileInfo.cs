fea9e572f56cd07fba1cf15f96ce96e18d3bdf96
Matthew Elliott <m.elliott091091@gmail.com>
2025-04-10 13:39:20 +0100

    Created ImageFileInfo class and started Fileservice.cs

---
+++ NeilSeniorBirdWalks/Models/ImageFileInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace NeilSeniorBirdWalks.Models
+{
+    public class ImageFileInfo
+    {
+        public int Id { get; set; }
+        public string FileName { get; set; } = string.Empty;
+        public string FilePath { get; set; } = string.Empty;
+        public DateTime UploadDate { get; set; }
+        public string? Description { get; set; } = string.Empty;
+        
+    }
+}
---
+++ NeilSeniorBirdWalks/Services/FileService.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+using System.Runtime.CompilerServices;
+
+namespace NeilSeniorBirdWalks.Services
+{
+    public class FileService
+    {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        private const string UploadsFolder = "uploads";
+        private const string ImagesFolder = "images";
+
+        public FileService(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+
+        public async Task<string> SaveFileAsync(IBrowserFile file)
+        {
+            if (file == null)
+            {
+                return string.Empty;
+            }
+            else
+            {
+                //Uploads the file to the Uploads folder
+                var uploadPath = Path.Combine(_webHostEnvironment.WebRootPath, ImagesFolder, UploadsFolder);
+                Directory.CreateDirectory(uploadPath);
+
+                var fileName = $"{Guid.NewGuid()}_{file.Name}";
+                var filePath = Path.Combine(uploadPath, fileName);
+
+                using (var fileStream = new FileStream(filePath, FileMode.Create))
+                {
+                    await file.OpenReadStream(maxAllowedSize: 10485760).CopyToAsync(fileStream);
+                }
+                // Returns the relative path to the file to save in the database
+                return $"/{ImagesFolder}/{UploadsFolder}/{fileName}";
+            }
+        }
+
+    }
+}
